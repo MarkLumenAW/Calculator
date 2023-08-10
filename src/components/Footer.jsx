40253c1cd5fd7631601bc05b9c118ee441e4e169
Alex Wang <67306827+MarkLumenAW@users.noreply.github.com>
2023-08-10 10:56:35 +1000

    Update LinkedIn

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer(props) {
         <a href="https://github.com/MarkLumenAW/FCC_Frontend_Project_4_Calculator" target="_blank" rel="noopener noreferrer">
           <Icon icon="mdi:github" color="#888" width="1rem" />
         </a>
-        <a href="https://www.linkedin.com/in/pengcheng-wang-0412b0151/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/alexwang-au/" target="_blank" rel="noopener noreferrer">
           <Icon icon="mdi:linkedin" color="#888" width="1rem" />
         </a>
       </div>
